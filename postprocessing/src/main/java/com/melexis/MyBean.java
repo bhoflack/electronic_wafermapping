b50532c346944abc004efb6a86659012b9916af1
Brecht Hoflack <brh@melexis.com>
2011-02-07 15:24:49 +0100

    Initial version

---
+++ postprocessing/src/main/java/com/melexis/MyBean.java
@@ -0,0 +1,22 @@
+package com.melexis;
+
+import org.apache.servicemix.jbi.listener.MessageExchangeListener;
+
+import javax.annotation.Resource;
+import javax.jbi.messaging.DeliveryChannel;
+import javax.jbi.messaging.ExchangeStatus;
+import javax.jbi.messaging.MessageExchange;
+import javax.jbi.messaging.MessagingException;
+
+public class MyBean implements MessageExchangeListener {
+
+    @Resource
+    private DeliveryChannel channel;
+
+    public void onMessageExchange(MessageExchange exchange) throws MessagingException {
+        System.out.println("Received exchange: " + exchange);
+        exchange.setStatus(ExchangeStatus.DONE);
+        channel.send(exchange);
+    }
+
+}
