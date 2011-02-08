e3c6d2dddc33ad95db78e2626511bf895735973e
Brecht Hoflack <brecht.hoflack@gmail.com>
2011-02-08 11:35:20 +0100

    Added TH01 route.

--- message.sh
+++ message.sh
@@ -1,19 +1,37 @@
 #!/bin/sh
 
-tempfile=`mktemp -t postprocess_job_XXXXXXXX`
+if [ "postprocessing" = "$1" ]; then
+    # Send a postprocessing message to the shopfloor interface
+    tempfile=`mktemp -t postprocess_job_XXXXXXXX`
 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" >> $tempfile
-echo "  <event>" >> $tempfile
-echo "    <type>entered_postprocessing</type>" >> $tempfile
-echo "    <lotname>A84001</lotname>" >> $tempfile
-echo "    <item>201234500</item>" >> $tempfile
-echo "    <when>20110207-16:02</when>" >> $tempfile
-echo "    <operator>brh</operator>" >> $tempfile
-echo "    <probing-site>IEPER</probing-site>" >> $tempfile
-echo "    <wafers>" >> $tempfile
-echo "      <wafer>1</wafer>" >> $tempfile
-echo "      <wafer>2</wafer>" >> $tempfile
-echo "    </wafers>" >> $tempfile
-echo "  </event>" >> $tempfile
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" >> $tempfile
+    echo "  <event>" >> $tempfile
+    echo "    <type>entered_postprocessing</type>" >> $tempfile
+    echo "    <lotname>A84001</lotname>" >> $tempfile
+    echo "    <item>201234500</item>" >> $tempfile
+    echo "    <when>20110207-16:02</when>" >> $tempfile
+    echo "    <operator>brh</operator>" >> $tempfile
+    echo "    <probing-site>IEPER</probing-site>" >> $tempfile
+    echo "    <wafers>" >> $tempfile
+    echo "      <wafer>1</wafer>" >> $tempfile
+    echo "      <wafer>2</wafer>" >> $tempfile
+    echo "    </wafers>" >> $tempfile
+    echo "  </event>" >> $tempfile
 
-mv $tempfile /tmp/postprocessing
+    mv $tempfile /tmp/postprocessing/
+elif [ "rtwm" = "$1" ]; then
+    # Send a wafermap finished message to the rtwm interface
+    tempfile=`mktemp -t rtwm_job_XXXXXXXX`
+
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" >> $tempfile
+    echo "  <event>" >> $tempfile
+    echo "    <lotname>A97123</lotname>" >> $tempfile
+    echo "    <waferid>1</waferid>" >> $tempfile
+    echo "    <when>20110208-11:15</when>" >> $tempfile
+    echo "    <probing-site>IEPER</probing-site>" >> $tempfile
+    echo "  </event>" >> $tempfile
+
+    mv $tempfile /tmp/rtwm/
+else
+    echo "Usage: $0 [postprocessing | rtwm]"
+fi
--- th01convertRoute/src/main/java/com/melexis/TH01RouteBuilder.java
+++ th01convertRoute/src/main/java/com/melexis/TH01RouteBuilder.java
@@ -4,13 +4,12 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
-public class Route extends RouteBuilder {
+public class TH01RouteBuilder extends RouteBuilder {
 
     private final static String NS = "jbi:endpoint:http://melexis.com/electronicwafermapping/";
 
     public void configure() {
-        // Send all lots that are moved in Oracle to the postprocessing
-        from(NS + "lot_moved_to_postprocessing/route")
-            .to(NS + "postprocesslot/postprocessing");
+        from(NS + "wafermap-saved/route")
+            .to(NS + "th01/convert");
     }
 }
